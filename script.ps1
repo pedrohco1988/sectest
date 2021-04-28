5923c899813f02d4d5e25ccc5a500d408bb620ef
pedrohco1988 <60531018+pedrohco1988@users.noreply.github.com>
2021-04-28 18:54:22 -0300

    script.ps1 para escaner de knockports

---
+++ script.ps1
@@ -0,0 +1,15 @@
+﻿param ($ip)
+if(!$ip){
+    echo "######## PORTSCAN knock #############"
+    echo "#####################################"
+    echo "Modo de usar .\script.ps1 192.168.0.1"
+} else {
+$topports = 13,37,30000,3000,1337
+try {foreach ($porta in $topports){
+if (Test-NetConnection $ip -Port $porta -WarningAction SilentlyContinue -InformationLevel Quiet){
+    echo "Porta $porta Aberta"
+}} else {
+    echo "Porta $porta Fechada"
+}} catch {}
+}
+
